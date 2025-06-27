f4837988bdcb67512ef17dc9ab5059926edaa6df
Song Eric Yu Li <ericli3690@gmail.com>
2025-06-27 18:51:32 -0400

    Updates

--- assets/index-DaLt-tlW.js
+++ assets/index-DaLt-tlW.js
@@ -3877,11 +3877,11 @@ void main() {
 [n][n]
 [t][t]Hi, I’m Eric! I’m entering my second year in Computer Science at the University of Waterloo this fall and am extremely interested in an internship at Toma. I learned about your company through Gautham and became intrigued after he showed me a demo of your product. To cut to the chase:
 [n][n]
-[t][t][b:1.] I have experience delivering [b:high-quality work]. I’m currently interning at Ford, working on developing reliable new features for the SYNC5 infotainment system. In my spare time, I’m designing the notifications system for a multi-million-download open-source Android mobile app through the Google Summer of Code program.
+[t][t][b:1.] I have experience delivering [b:high-quality work]. I’m currently interning at Ford, working on developing reliable new features for the SYNC5 infotainment system. In my spare time, I’m designing the notifications system for the multi-million-download open-source [i:Anki] Android mobile app through the Google Summer of Code program.
 [n][n]
-[t][t][b:2.] I have experience delivering [b:high-speed work]. Gautham and I have won four hackathons together.
+[t][t][b:2.] I have experience delivering [b:high-speed work]. Gautham and I have won four hackathons together. Project descriptions attached below.
 [n][n]
-[t][t][b:3.] I have experience delivering [b:high-variability work]. I’m adaptable and have strong coordination skills. Gautham and I once co-founded a hackathon that garnered $2000+ in corporate prize sponsorships and drew over 100 participants.
+[t][t][b:3.] I have experience delivering [b:high-variability work]. I’m adaptable and have strong coordination skills. Gautham and I once co-founded a hackathon that garnered $2000+ in corporate prize sponsorships from companies like Wolfram Alpha and Postman, drawing over 100 participants.
 [n][n]
 [t][t][b:4.] But most of all, I’m ready to deliver [b:high-dedication work]. I believe my greatest weakness right now is that my resume doesn't have enough machine learning experiences on it. Luckily, the next time I’m free for an internship is January or May 2026, and I haven’t decided what side project to devote myself to for this fall yet…
 [n][n]
